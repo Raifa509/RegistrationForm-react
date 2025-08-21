623d6a85c3bf4871a03b17d74ec0ec2422aedf36
Raifa509 <fathimathulraifanp@gmail.com>
2025-08-21 23:36:59 +0400

    edited design

--- src/Form.jsx
+++ src/Form.jsx
@@ -4,6 +4,7 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
 
 
 function Form() {
@@ -23,16 +24,16 @@ function Form() {
                             </div>
                         </div>
 
-                        <div className="col-md-7 px-5 py-3">
-                           <div className='ms-4'>
+                        <div className="col-md-7 px-5 py-1">
+                            <div className='ms-4'>
                                 <h5 className='text-center pb-3 '>Fill the Form</h5>
-                                <input type="text" className="form-control mb-3" placeholder='Full Name' />
-                                <input type="date" className="form-control mb-3" />
-                                <input type="email" className="form-control mb-3" placeholder='Email ID' />
-                                <input type="phone" className="form-control mb-3" placeholder='+91 XXXXXXXXXX' />
-    
+                                <input type="text" className="form-control mb-3 glass-input" placeholder='Full Name' />
+                                <input type="date" className="form-control mb-3  glass-input" />
+                                <input type="email" className="form-control mb-3  glass-input" placeholder='Email ID' />
+                                <input type="phone" className="form-control mb-3  glass-input" placeholder='+91 XXXXXXXXXX' />
+
                                 {/* radio button */}
-                                <FormLabel id="demo-row-radio-buttons-group-label" className='text-dark radio-style'>Gender</FormLabel>
+                                <FormLabel id="demo-row-radio-buttons-group-label" className='text-dark radio-style '>Gender</FormLabel>
                                 <RadioGroup
                                     row
                                     aria-labelledby="demo-row-radio-buttons-group-label"
@@ -41,13 +42,13 @@ function Form() {
                                     <FormControlLabel className='radio-style' value="male" control={<Radio />} label="Male" />
                                     <FormControlLabel className='radio-style' value="other" control={<Radio />} label="Other" />
                                 </RadioGroup>
-    
-    
-                                <label htmlFor="" className='radio-style'>Address</label>
-                                <textarea className='form-control mt-2' name="" id=""></textarea>
-    
+
+
+                                <label htmlFor="" className='radio-style '>Address</label>
+                                <textarea className='form-control mt-2  glass-input' name="" id=""></textarea>
+
                                 {/* dropdown */}
-                                <select class="form-select mt-3" aria-label="Default select example">
+                                <select className="form-select mt-3 glass-input" aria-label="Default select example" id='mydropdown'>
                                     <option selected>Choose the Course</option>
                                     <option value="1">Biology</option>
                                     <option value="2">Computer Science</option>
@@ -55,13 +56,15 @@ function Form() {
                                     <option value="4">Humanities</option>
                                 </select>
 
-                               <div className='text-center mt-4'>
-                                    <div className="btn btn-success me-4 px-4 py-2">Submit</div>
-                                    <div className="btn btn-outline-danger px-4 py-2">Reset</div>
-                               </div>
+
+                                <div className='text-center mt-4'>
+                                    <div className="btn me-4 px-4 py-2" style={{ backgroundColor: '#8e44ad', color: 'white' }}>Submit</div>
+                                    <div className="btn px-4 py-2" style={{ backgroundColor: '#e67e22', color: 'white' }}>Reset</div>
+
+                                </div>
 
 
-                           </div>
+                            </div>
 
                         </div>
                     </div>
